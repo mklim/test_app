62de4080dbd3c40d26ab800c3f1c0233f0e7f8f5
Michael Klimushyn <mklim@google.com>
2020-02-11 11:17:00 -0800

    Add FLAG_SECURE settings

--- android/app/src/main/kotlin/com/example/test_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/test_app/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.test_app
 
 import android.os.Bundle
+import android.view.WindowManager
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
@@ -8,5 +9,6 @@ class MainActivity: FlutterActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     GeneratedPluginRegistrant.registerWith(this)
+    this.window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
   }
 }
