4a14065077ffce988f046c60e1e2c5d530805145
Michael Klimushyn <mklim@google.com>
2020-02-11 13:47:34 -0800

    Add setSecure call to the SurfaceView

--- android/app/src/main/kotlin/com/example/test_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/test_app/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.test_app
 
+import android.os.Build
 import android.os.Bundle
 import android.view.WindowManager
+import androidx.annotation.RequiresApi
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+  @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     GeneratedPluginRegistrant.registerWith(this)
+    flutterView.setSecure(true)
     this.window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
   }
 }
